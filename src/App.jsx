a99d4b6d0b447676018fa9c844e85839e614cbb3
slava <75099655+SlavaKuntsov@users.noreply.github.com>
2024-07-23 05:37:51 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<h1 className='bg-red-300 text-3xl'>Hello</h1>
+		</>
+	);
+}
+
+export default App;
